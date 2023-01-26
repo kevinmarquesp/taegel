967a9d82919f2182c365f71b2506ca7ea607aebc
Kevin Marques <kevinmarqueskmp@gmail.com>
2023-01-26 09:21:55 -0300

    refactor: moved the progress loop update to a separeted function

--- taegel/views/progress.py
+++ taegel/views/progress.py
@@ -1,6 +1,6 @@
 import rich.progress
 
-from rich.progress import Progress
+from rich.progress import Progress, TaskID
 from multiprocessing.connection import Connection
 from typing import Any, Callable
 
@@ -37,10 +37,27 @@ def idle(description: str) -> Callable:
     return decorator
 
 
-def downloading_log(ppipe: Connection, prog: Progress) -> None:
+def downloading_handler(ppipe: Connection, prog: Progress, task: TaskID) -> None:
+    """Log handler for the :py:meth:`downloading` progress bar, this recives
+    the info sended throught the *parent pipe* and update the task with that.
+
+    :param Connection ppipe: Parent pipe to recive the log status.
+    :param Progress prog: Progress bar to update and display the log messages.
+    :param TaskID task: Task to be updated.
     """
-    """
-    pass
+    status: dict = ppipe.recv()
+    log: Callable = prog.console.print
+
+    if status['done']:
+        prog.update(task, advance=1)
+
+    if status['is_ok']:
+        log(f"Downloaded [cyan]{status['info']}[/] with success! [black]{status['url']}[/]")
+    else:
+        log(f"{status['info']}, sorry... {status['url']}", style='red')
+
+    if status['desc'] is not None:
+        log(status['desc'])
 
 
 def downloading(ppipe: Connection, total: int, done: int) -> None:
@@ -51,23 +68,10 @@ def downloading(ppipe: Connection, total: int, done: int) -> None:
     :param int total: Total steps to comple the progress bar.
     """
     with rich.progress.Progress(*progress_config, transient=True) as prog:
-        task_download = prog.add_task('Downloading...', total=total)
-        prog.update(task_download, advance=done)
+        task: TaskID = prog.add_task('Downloading...', total=total)
+        prog.update(task, advance=done)
 
-        prog.console.print('[on green] [/] It may take a while, be patient! :coffee:\n')
+        prog.console.print('[on green] [/] It may take a while, be patient! :coffee:')
 
         while not prog.finished:
-            status: dict = ppipe.recv()
-
-            if status['done']:
-                prog.update(task_download, advance=1)
-
-            if status['is_ok']:
-                prog.console.print(f"Downloaded [cyan]{status['info']}[/] with success! [black]{status['url']}[/]")
-
-            else:
-                prog.console.print(f"{status['info']}, sorry... {status['url']}",
-                                   style='red')
-
-            if status['desc'] is not None:
-                prog.console.print(status['desc'])
+            downloading_handler(ppipe, prog, task)
