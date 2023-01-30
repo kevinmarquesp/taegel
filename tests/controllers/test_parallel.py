f955be6f5bddd74cb657c157993f59d21a2cd394
Kevin Marques <kevinmarqueskmp@gmail.com>
2023-01-30 10:05:32 -0300

    test(ref): tweaking the env options implementation

--- tests/controllers/test_parallel.py
+++ tests/controllers/test_parallel.py
@@ -8,8 +8,8 @@ import tests
 from taegel.models.types import AlbumInfo
 
 
-def test_interface_download_handler_progress_communication():
-    if os.getenv('SKIP_INTERFACE') not in ['false', 'False', 'FALSE', '0']:
+def test_interface_download_handler_progress_communication() -> None:
+    if os.getenv('SKIP_INTERFACE') in ['true', '1']:
         views.log.warning('skiping interface test')
         return
 
@@ -18,9 +18,9 @@ def test_interface_download_handler_progress_communication():
     album: AlbumInfo = models.types.AlbumInfo(target=tmp_target, sources=[
         'https://www.youtube.com/watch?v=Ne0_v4Yadmm',  # invalid link
         'https://www.youtube.com/watch?v=Ne0_v4YadmM',  # valid link
-        'https://inv.bp.projectsegfau.lt/watch?v=DWYMyp638PQ&list=PLOttxkyjhZeIwNpyDKV0HIRltQaHrzMm3&index=1',  # invidius link
     ])
 
     ctr.parallel.download_handler(album, 2, len(album.sources))
 
-    tests.remove_tmp_directory(tmp_target)
+    if os.getenv('DELETE_TMP') not in ['false', '0']:
+        tests.remove_tmp_directory(tmp_target)
